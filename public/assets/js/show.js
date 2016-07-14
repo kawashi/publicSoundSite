6555f5024133871e5a889b3fa804d8d7b0306b23
Marihuana <allen@ryoheikawashima-no-MacBook-Pro.local>
2016-07-14 11:40:00 +0900

    JS をオブジェクト指向で書き換え (未完了)

--- public/assets/js/show.js
+++ public/assets/js/show.js
@@ -1,53 +1,51 @@
-var play_flag = 0;
-var dl_flag   = 0;
-
 $(function(){
-    /* ダウンロード数カウント */
-    $(".downloads a").on("click",function(){
-        if( dl_flag === 0 ){
-            dl_flag = 1;
-            $.get('dl_count',{name:$(this).data("name")},function(data){
-                console.log("ダウンロードカウントアップ");
-            });
+    var play_flag = 0;
+    var dl_flag   = 0;
+    /* クリックされた回数を DB に記録 */
+    var ClickCounter = (function(){
+        function ClickCounter(action){
+            this.done_count  = false;
+            this.action_name = action + "_count";
+            this.listener();
         }
-    });
-    
-    /* 再生数カウント */
-    $(".audio").on("play",function(){
-        if( play_flag === 0 ){
-            play_flag = 1;
-            $.get('play_count',{name:$(this).data("name")},function(data){
-               console.log(data); 
-            });
-        }        
-    });
-    
+        ClickCounter.prototype.count = function(){
+            if( !this.done_count ){
+                $.get(this.action_name, {name:$(this).data("name")}, function(data){this.done_count = true;});
+            }
+        }
+        ClickCounter.prototype.listener = function(){
+            $('.downloads a').on("click", this.count);
+        }
+    })();
+
     /* コメント送信 */
     // TOOD:要リファクタ
     $(".comment_submit").on("click",function(){
-        var $comment_group = $(this).parent();
-        console.log($comment_group);
-        var $text_area     = $comment_group.children("div").children();    
-        console.log($text_area);
-        
-        var comment = $text_area.val();
-        var name    = $(this).closest('.comment').data('name');
-        
-        // コメント表示
-        var $parent = $(this).closest(".sound");
-        $comment_list = $parent.children('.comment_list');
-        $comment_list.append("<div></div");
-        var text = '<p>' + comment + '</p>';
-        var $child  = $comment_list.children("div:last-child");
-        $child.attr({class: 'message'}).append(text);
+        // 理想は、入力したコメント取得し、DBにわたし、画面に表示
+        var comment   = $(".comment_form").val();
+        var data_name = $(".comment").data("name");
+        var add_text  = '<p>' + comment + '</p>';
         
         
-        // フォームリセット
+        // TDOO: 曲ごとの場所を特定しなきゃダメだからここは違う
+        // TODO: this からたどる
+        $("#comments").append(add_text);
+
+        
+            
+        //$.get('send_comment', {comment: comment,sound_name: data_name}, function(data){
+        //     console.log("成功");
+        // })
+
         $(".comment_form").val("");
-        
-        $.get('send_comment',{comment:comment,sound_name:name},function(data){   
-            console.log("成功");
-        })
+
+
     })
-    
+
+    // TODO:ボタンごとにnewすべきだと思う
+    dl_count   = new ClickCounter("dl");
+    play_count = new ClickCounter("play");
+
+    // 初期化
+
 });
