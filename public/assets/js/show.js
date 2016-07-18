b35f289cae52763afbec149e464b3ed063e063a7
Marihuana <allen@ryoheikawashima-no-MacBook-Pro.local>
2016-07-19 00:34:02 +0900

    コメント機能をオブジェクト指向でリファクタリング

--- public/assets/js/show.js
+++ public/assets/js/show.js
@@ -66,26 +66,59 @@ $(".audio").each(function(){
     new PlayCounter(this); 
 });
 
-/* コメント送信 */
-// TOOD:要リファクタ
-$(".comment_submit").on("click",function(){
-    // 理想は、入力したコメント取得し、DBにわたし、画面に表示
-    var comment   = $(".comment_form").val();
-    var data_name = $(".comment").data("name");
-    var add_text  = '<p>' + comment + '</p>';
+// ----- クラス定義 --------
+var Comment = (function(){
+    // コンストラクタ
+    function Comment(obj){
+        this.initialized(obj);
+        this.listener();
+    }
+    
+    // 変数初期化
+    Comment.prototype.initialized = function(obj){
+        this.$target       = $(obj);
+        this.$sound_dom    = $("#sound-id-" + this.$target.data("id"));
+        this.$comment_form = this.$sound_dom.find(".comment_form");
+        this.data_id       = this.$target.data("id");
+        // コメント数取得
+        var self = this;
+        $.get('get_comment_count', {data_id: this.data_id}, function(data){
+            self.comment_count = data;
+        });
+    }
+    
+    // イベントリスナー
+    Comment.prototype.listener = function(){
+        var self = this;
+        this.$target.on('click', function(){
+            self.send_message(self.$comment_form.val()); 
+            self.show_comment(self.$comment_form.val());
+            self.$comment_form.val("");
+        });
+    }
+    
+    // コメント送信
+    Comment.prototype.send_message = function(comment){
+        $.get('send_comment', {
+            comment: comment,
+            data_id: this.data_id
+        });
+    }
+    
+    // コメント表示
+    Comment.prototype.show_comment = function(comment){
+        var comment       = '<p class="comment_text">' + comment + '</p>';
+        if( this.comment_count < 3 ) this.$sound_dom.find(".comments").append(comment);
+        else                         this.$sound_dom.find(".show_comment_button").prev().after(comment);
+    }
+    
+    return Comment;
+})();
 
 
-    // TDOO: 曲ごとの場所を特定しなきゃダメだからここは違う
-    // TODO: this からたどる
-    $("#comments").append(add_text);
-
-
-
-    //$.get('send_comment', {comment: comment,sound_name: data_name}, function(data){
-    //     console.log("成功");
-    // })
-
-    $(".comment_form").val("");
-
+// ------ インスタンス化 ------
+$(".comment_submit").each(function(){
+    new Comment(this);
 });
+
 });
