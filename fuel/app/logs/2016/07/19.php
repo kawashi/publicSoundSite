fa9cdf3d158db481ac254c27758155030028aa8d
Marihuana <allen@ryoheikawashima-no-MacBook-Pro.local>
2016-07-31 15:57:14 +0900

    compass を導入して SCSS のコンパイルを自動化したかったが、うまく出来なかった

---
+++ fuel/app/logs/2016/07/19.php
@@ -0,0 +1,10 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2016-07-19 12:34:34 --> Fatal Error - Call to a member function count() on a non-object in /Users/allen/htdocs/publicSoundSite/fuel/packages/oil/classes/console.php(116) : eval()'d code on line 1
+ERROR - 2016-07-19 12:34:49 --> Fatal Error - Class 'Mode_PublicSound' not found in /Users/allen/htdocs/publicSoundSite/fuel/packages/oil/classes/console.php(116) : eval()'d code on line 1
+ERROR - 2016-07-19 12:35:00 --> Fatal Error - Class 'Mode_PublicSound' not found in /Users/allen/htdocs/publicSoundSite/fuel/packages/oil/classes/console.php(116) : eval()'d code on line 1
+ERROR - 2016-07-19 12:35:43 --> Fatal Error - Call to a member function count() on a non-object in /Users/allen/htdocs/publicSoundSite/fuel/packages/oil/classes/console.php(116) : eval()'d code on line 1
+ERROR - 2016-07-19 12:36:26 --> Fatal Error - Call to a member function count() on a non-object in /Users/allen/htdocs/publicSoundSite/fuel/packages/oil/classes/console.php(116) : eval()'d code on line 1
+ERROR - 2016-07-19 12:36:37 --> Fatal Error - Call to a member function count() on a non-object in /Users/allen/htdocs/publicSoundSite/fuel/packages/oil/classes/console.php(116) : eval()'d code on line 1
+ERROR - 2016-07-19 12:39:27 --> Fatal Error - Call to undefined function push() in /Users/allen/htdocs/publicSoundSite/fuel/packages/oil/classes/console.php(116) : eval()'d code on line 1
+ERROR - 2016-07-19 15:46:26 --> Notice - Undefined variable: sound_id in /Users/allen/htdocs/publicSoundSite/fuel/app/views/publicsound/show/main.php on line 136
---
+++ fuel/app/logs/2016/07/20.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2016-07-20 11:43:03 --> Parsing Error - parse error in /Users/allen/htdocs/publicSoundSite/fuel/packages/oil/classes/console.php(116) : eval()'d code on line 1
+ERROR - 2016-07-20 11:55:20 --> 23000 - SQLSTATE[23000]: Integrity constraint violation: 1048 Column 'user_id' cannot be null with query: "INSERT INTO `comments` (`sound_id`, `message`, `date`, `created_at`, `updated_at`, `user_id`) VALUES ('4', '感動的な曲ですぅ\n', '2016/07/31', 1468983320, null, null)" in /Users/allen/htdocs/publicSoundSite/fuel/core/classes/database/pdo/connection.php on line 272
